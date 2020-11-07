480af3dd0bd1b2bf95229238db389534b26dfa0e
Yamunasatheesh3 <71799576+Yamunasatheesh3@users.noreply.github.com>
2020-11-07 16:44:26 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,13 +1,35 @@
+import json
+import googlemaps
+import pprint
+import time
+
+
 from mycroft import MycroftSkill, intent_file_handler
+from adapt.intent import IntentBuilder
+from mycroft.skills.core import MycroftSkill
+from mycroft.util.log import getLogger
 
 
 class Poi(MycroftSkill):
     def __init__(self):
-        MycroftSkill.__init__(self)
+         super(Poi, self).__init__(name="Poi")
+      #  MycroftSkill.__init__(self)
 
+     def initialize(self):
+             poi_intent = IntentBuilder("PoiIntent").require("PoiKeyword").build()
+             self.register_intent(poi_intent, self.handle_poi_intent)
+            
     @intent_file_handler('poi.intent')
     def handle_poi(self, message):
-        self.speak_dialog('poi')
+        api_key = 'AIzaSyBp25k9LqhGDh4nAIHeFnhu045jrWPnWkg'
+        gmaps = googlemaps.Client(key = api_key)
+        places_result = gmaps.places_nearby(location= '45.421532,-75.697189', radius = 10000, open_now = False, type = 'gas_station')
+        
+        for place in places_result['results']:
+           my_place_id = place['place_id']
+           my_fields = ['name', 'formatted_phone_number', 'type', 'formatted_address']
+           place_details = gmaps.place(place_id = my_place_id, fields = my_fields)
+        self.speak_dialog('place_details')
 
 
 def create_skill():
