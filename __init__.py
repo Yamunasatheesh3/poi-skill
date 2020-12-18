1723df021e461072601d8c7ab633ef85c1f9c1d2
Yamunasatheesh3 <71799576+Yamunasatheesh3@users.noreply.github.com>
2020-12-18 12:06:50 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -72,7 +72,7 @@ class Poi(MycroftSkill):
         latlong = message.data.get(places_nearby['geo_loc'])
         api_root = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json'
         api_params = '?location=' + latlong +\
-                     '&radius=' + 50000 +\
+                     '&radius=' + 10000 +\
                      '&type=formatted_address' +\
                      '&key=' + api_key
         api_url = api_root + api_params
